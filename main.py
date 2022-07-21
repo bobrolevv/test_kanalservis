48a5c97b85902036a482cd8354cd672f106d66c2
art <bobrolevv@yandex.ru>
2022-07-21 23:02:43 +0700

    init

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # https://docs.google.com/spreadsheets/d/1f-qZEX1k_3nj5cahOzntYAnvO4ignbyesVO7yuBdv_g/edit?usp=sharing
 
 def print_hi(name):
-
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
