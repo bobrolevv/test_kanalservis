bf24c75b2962d269518db79b3510244738c6859d
art <bobrolevv@yandex.ru>
2022-07-21 23:01:47 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# https://docs.google.com/spreadsheets/d/1f-qZEX1k_3nj5cahOzntYAnvO4ignbyesVO7yuBdv_g/edit?usp=sharing
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+
+
